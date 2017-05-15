fc4c523de1d7211803403cd332e45f6a72d0635c
SonGokussj4 <SonGokussj4@centrum.cz>
2017-05-15 12:56:48 +0200

    added FontAwesome and JQuery as offline static files; experiments with modal in issues_status.html

--- beta-issues/static/js/custom.js
+++ beta-issues/static/js/custom.js
@@ -13,3 +13,18 @@ $('#loading-forever-btn').click(function () {
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+
+// $('#issueStatus_editModal').on('show.bs.modal', function (event) {
+//   var button = $(event.relatedTarget) // Button that triggered the modal
+//   var iid = button.data('id') // Extract info from data-* attributes
+//   var row_data = button.data('row_data')
+//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//   var modal = $(this)
+//   modal.find('.modal-title').text('ID: ' + iid)
+//   modal.find('.modal-body span').text(row_data)
+// })
+
+
+
