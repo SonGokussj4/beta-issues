c39e88f4bca6779571d004ab2e953a6dc162c7f4
SonGokussj4 <SonGokussj4@centrum.cz>
2017-05-23 20:51:41 +0200

    added table of all uploaded resolved issues in DB to upload_release_changes.html

--- beta-issues/__init__.py
+++ beta-issues/__init__.py
@@ -194,12 +194,13 @@ def edit_issue():
 def upload_release_changes():
     cur, db = get_db(cursor=True)
     data = cur.execute("SELECT * FROM resolved_issues")
-    issues = len(data.fetchall())
+    issues_db = data.fetchall()
+    issues = len(issues_db)
 
     data = cur.execute("SELECT version, COUNT(1) FROM resolved_issues GROUP BY version ORDER BY version DESC")
     nums = [dict(num) for num in data.fetchall()]
 
-    return render_template('upload_release_changes.html', resolved_issues_in_db=issues, nums=nums)
+    return render_template('upload_release_changes.html', resolved_issues_in_db=issues, nums=nums, issues_db=issues_db)
 
 
 @app.route('/upload_release_changes/', methods=['GET', 'POST'])
